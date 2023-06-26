0be86ddb75f1a0419db4fcac80143907e98b2443
mathoumin <maxime.thoumin@etu.uca.fr>
2023-06-26 16:42:30 +0200

    pave serpent fenetre et rideaux

--- MaximeTPerso/pave_serpents_Maxime/pave_serpents_Maxime.c
+++ MaximeTPerso/pave_serpents_Maxime/pave_serpents_Maxime.c
@@ -3,17 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
-/*********************************************************************************************************************/
-/*                              Programme d'exemple de création de rendu + dessin                                    */
-/*********************************************************************************************************************/
-void end_sdl(char ok,                                // fin normale : ok = 0 ; anormale ok = 1
-    char const* msg,                                 // message à afficher
-    SDL_Window* window,                              // fenêtre à fermer
-    SDL_Renderer* renderer) {                        // renderer à fermer
+void end_sdl(char ok,                            // fin normale : ok = 0 ; anormale ok = 1
+    char const* msg,                             // message à afficher
+    SDL_Window* window,                          // fenêtre à fermer
+    SDL_Renderer* renderer) {                    // renderer à fermer
         char msg_formated[255];                                            
-        int l;                                                     
+        int l;
                                        
-        if (!ok) {                                   // Affichage de ce qui ne va pas
+        if (!ok) {                               // Affichage de ce qui ne va pas
             strncpy(msg_formated, msg, 250);                                         
             l = strlen(msg_formated);                                            
             strcpy(msg_formated + l, " : %s\n");                                     
@@ -36,80 +33,74 @@ void end_sdl(char ok,                                // fin normale : ok = 0 ; a
         if (!ok) {                                       // On quitte si cela ne va pas            
             exit(EXIT_FAILURE);                                                  
         }                                                          
-        }                                                        
+}                                                        
                                                 
-        void draw(SDL_Renderer* renderer) {            // Je pense que vous allez faire moins laid :)
-        SDL_Rect rectangle;                                                
+void draw(SDL_Renderer* renderer) {            // Je pense que vous allez faire moins laid :)
+    SDL_Rect rectangle;                                                
                                                 
-        SDL_SetRenderDrawColor(renderer,                                       
-                    50, 0, 0,                             // mode Red, Green, Blue (tous dans 0..255)
+    SDL_SetRenderDrawColor(renderer,                                       
+                    250, 0, 0,                             // mode Red, Green, Blue (tous dans 0..255)
                     255);                                 // 0 = transparent ; 255 = opaque
-        rectangle.x = 0;                                             // x haut gauche du rectangle
-        rectangle.y = 0;                                                  // y haut gauche du rectangle
-        rectangle.w = 400;                                                // sa largeur (w = width)
-        rectangle.h = 400;                                                // sa hauteur (h = height)
+    rectangle.x = 0;                                             // x haut gauche du rectangle
+    rectangle.y = 0;                                                  // y haut gauche du rectangle
+    rectangle.w = 500;                                                // sa largeur (w = width)
+    rectangle.h = 600;                                                // sa hauteur (h = height)
                                                 
-        SDL_RenderFillRect(renderer, &rectangle);                        
+    SDL_RenderFillRect(renderer, &rectangle);                        
                                                 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);                   
-        SDL_RenderDrawLine(renderer,                             
-                0, 0,                                   // x,y du point de la première extrémité
-                400, 400);                              // x,y seconde extrémité
-                                                
-
-        /* tracer un cercle n'est en fait pas trivial, voilà le résultat sans algo intelligent ... */
-        for (float angle = 0; angle < 2 * M_PI; angle += M_PI / 4000) {      
-        SDL_SetRenderDrawColor(renderer,
-                    (cos(angle * 2) + 1) * 255 / 2,          // quantité de Rouge      
-                    (cos(angle * 5) + 1) * 255 / 2,          //          de vert 
-                    (cos(angle) + 1) * 255 / 2,              //          de bleu
-                    255);                                    // opacité = opaque
-        SDL_RenderDrawPoint(renderer,                  
-                    200 + 100 * cos(angle),                     // coordonnée en x
-                    200 + 150 * sin(angle));                    //            en y   
-        }
-    }
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);                   
+    SDL_RenderDrawLine(renderer,                             
+        0, 0,                                   // x,y du point de la première extrémité
+        400, 400);                              // x,y seconde extrémité
+    
+}
 
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
 
+    int temps = 0;
+
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
-    SDL_DisplayMode screen;
 
-/*************************************************************************************************/  
-/*               Initialisation de la SDL  + gestion de l'échec possible                         */
-/*************************************************************************************************/  
+    SDL_Rect rideau_gauche;
+    SDL_Rect rideau_droite;
+
+    /* Initialisation SDL */
     if (SDL_Init(SDL_INIT_VIDEO) != 0) end_sdl(0, "ERROR SDL INIT", window, renderer);
 
-        SDL_GetCurrentDisplayMode(0, &screen);
-        printf("Résolution écran\n\tw : %d\n\th : %d\n",
-            screen.w, screen.h);
+    /* Création de la fenêtre */
+    window = SDL_CreateWindow("Théâtre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 600, SDL_WINDOW_OPENGL);
+    if (window == NULL) end_sdl(0, "ERROR WINDOW CREATION", window, renderer);
 
-       /* Création de la fenêtre */
-       window = SDL_CreateWindow("Premier dessin",
-                 SDL_WINDOWPOS_CENTERED,
-                 SDL_WINDOWPOS_CENTERED, screen.w * 0.66,
-                 screen.h * 0.66,
-                 SDL_WINDOW_OPENGL);
-       if (window == NULL) end_sdl(0, "ERROR WINDOW CREATION", window, renderer);
+    /* Création du renderer */
+    renderer = SDL_CreateRenderer(window, -1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) end_sdl(0, "ERROR RENDERER CREATION", window, renderer);
 
-       /* Création du renderer */
-       renderer = SDL_CreateRenderer(window, -1,
-                     SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-       if (renderer == NULL) end_sdl(0, "ERROR RENDERER CREATION", window, renderer);
+    /* Dessin */
+    for(temps = 0; temps < 100; temps+=1) {
 
-       /*********************************************************************************************************************/
-       /*                                     On dessine dans le renderer                                                   */
-       /*********************************************************************************************************************/
-       /*             Cette partie pourrait avantageusement être remplacée par la boucle évènementielle                     */ 
-       draw(renderer);                                      // appel de la fonction qui crée l'image  
-       SDL_RenderPresent(renderer);                         // affichage
-       SDL_Delay(1000);                                     // Pause exprimée en ms
-                                                       
+        SDL_SetRenderDrawColor(renderer, 250, 0, 0, 255);                     
+        rideau_gauche.x = 0;
+        rideau_gauche.y = 0;
+        rideau_gauche.w = 500;                                      
+        rideau_gauche.h = 600;
+        SDL_RenderFillRect(renderer, &rideau_gauche);
 
-       /* on referme proprement la SDL */
-       end_sdl(1, "Normal ending", window, renderer);
-       return EXIT_SUCCESS;
-     }
+        SDL_SetRenderDrawColor(renderer, 250, 0, 0, 255);                     
+        rideau_droite.x = 500;
+        rideau_droite.y = 0;
+        rideau_droite.w = 500;                                      
+        rideau_droite.h = 600;
+        SDL_RenderFillRect(renderer, &rideau_droite);
+    }
+
+    SDL_RenderPresent(renderer);                         // affichage
+    SDL_Delay(5000);                                     // Pause exprimée en ms
+                          
+    /* Fermeture SDL */
+    end_sdl(1, "Normal ending", window, renderer);
+
+    return EXIT_SUCCESS;
+}
