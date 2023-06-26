968d1e38a22d031de6a5181b0a5fdd5536acd6a8
Florian BOISSET <florian.boisset@etu.uca.fr>
2023-06-26 15:44:37 +0200

    bonhomme qui bouge, erreu seg

--- FlorianBPerso/fenetre.c
+++ FlorianBPerso/fenetre.c
@@ -1,64 +1,204 @@
-     #include <SDL2/SDL.h>
-     #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
-     /************************************/
-     /*  exemple de création de fenêtres */
-     /************************************/
+/************************************/
+/*  exemple de création de fenêtres */
+/************************************/
 
-     int main(int argc, char **argv) {
-       (void)argc;
-       (void)argv;
+int main(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
 
-       SDL_Window 
-            *window_1 = NULL,                     // Future fenêtre de gauche
-        *window_2 = NULL;                     // Future fenêtre de droite
+  SDL_Window
+      *Tete = NULL,
+      *Cou = NULL,
+      *Corps = NULL,
+      *Bassin = NULL,
+      *JambeG = NULL,
+      *JambeD = NULL,
+      *Bras = NULL; // Future fenêtre de gauche
 
-       /* Initialisation de la SDL  + gestion de l'échec possible */
-       if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-     SDL_Log("Error : SDL initialisation - %s\n", 
-                  SDL_GetError());                // l'initialisation de la SDL a échoué 
-     exit(EXIT_FAILURE);
-       }
+  SDL_Renderer
+      *RendTete = NULL,
+      *RendCou = NULL,
+      *RendCorps = NULL,
+      *RendBassin = NULL,
+      *RendBras = NULL,
+      *RendJambeG = NULL,
+      *RendJambeD = NULL;
 
-       /* Création de la fenêtre de gauche */
-       window_1 = SDL_CreateWindow(
-       "Fenêtre à gauche",                    // codage en utf8, donc accents possibles
-       0, 0,                                  // coin haut gauche en haut gauche de l'écran
-       400, 300,                              // largeur = 400, hauteur = 300
-       SDL_WINDOW_RESIZABLE);                 // redimensionnable
+  /* Initialisation de la SDL  + gestion de l'échec possible */
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    SDL_Log("Error : SDL initialisation - %s\n",
+            SDL_GetError()); // l'initialisation de la SDL a échoué
+    exit(EXIT_FAILURE);
+  }
 
-       if (window_1 == NULL) {
-     SDL_Log("Error : SDL window 1 creation - %s\n", 
-                 SDL_GetError());                 // échec de la création de la fenêtre
-     SDL_Quit();                              // On referme la SDL       
-     exit(EXIT_FAILURE);
-       }
-       /* Création de la fenêtre de droite */
-       window_2 = SDL_CreateWindow(
-       "Fenêtre à droite",                    // codage en utf8, donc accents possibles
-       400, 0,                                // à droite de la fenêtre de gauche
-       500, 300,                              // largeur = 500, hauteur = 300
-       0);
+  /* Création de la fenêtre de gauche */
+  Tete = SDL_CreateWindow(
+      "Tete",                // codage en utf8, donc accents possibles
+      820, 100,                // coin haut gauche en haut gauche de l'écran
+      150, 150,              // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE); // redimensionnable
+  if (Tete == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendTete = SDL_CreateRenderer(Tete, -1, 0);
 
-       if (window_2 == NULL) {
-     /* L'init de la SDL : OK
-        fenêtre 1 :OK
-        fenêtre 2 : échec */
-     SDL_Log("Error : SDL window 2 creation - %s\n", 
-                 SDL_GetError());                 // échec de la création de la deuxième fenêtre 
-     SDL_DestroyWindow(window_1);             // la première fenétre (qui elle a été créée) doit être détruite
-     SDL_Quit();
-     exit(EXIT_FAILURE);
-       }
+  Cou = SDL_CreateWindow(
+      "Cou",    // codage en utf8, donc accents possibles
+      850, 250, // coin haut gauche en haut gauche de l'écran
+      90, 75, // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE);
+  if (Cou == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendCou = SDL_CreateRenderer(Cou, -1, 0);
 
-       /* Normalement, on devrait ici remplir les fenêtres... */
-       SDL_Delay(2000);                           // Pause exprimée  en ms
+  Corps = SDL_CreateWindow(
+      "Corps",  // codage en utf8, donc accents possibles
+      750, 340, // coin haut gauche en haut gauche de l'écran
+      300, 300, // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE);
+  if (Corps == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendCorps = SDL_CreateRenderer(Corps, -1, 0);
 
-       /* et on referme tout ce qu'on a ouvert en ordre inverse de la création */
-       SDL_DestroyWindow(window_2);               // la fenêtre 2  
-       SDL_DestroyWindow(window_1);               // la fenêtre 1     
+  Bassin = SDL_CreateWindow(
+      "Bassin", // codage en utf8, donc accents possibles
+      720, 640, // coin haut gauche en haut gauche de l'écran
+      350, 100, // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE);
+  if (Bassin == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendBassin = SDL_CreateRenderer(Bassin, -1, 0);
 
-       SDL_Quit();                                // la SDL
- 
-       return 0;
-     }
+  Bras = SDL_CreateWindow(
+      "Bras", // codage en utf8, donc accents possibles
+      650, 430, // coin haut gauche en haut gauche de l'écran
+      500, 50, // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE);
+  if (Bras == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendBras = SDL_CreateRenderer(Bras, -1, 0);
+
+  JambeG = SDL_CreateWindow(
+      "Jambe gauche", // codage en utf8, donc accents possibles
+      720, 740, // coin haut gauche en haut gauche de l'écran
+      100, 200, // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE);
+  if (JambeG == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendJambeG = SDL_CreateRenderer(JambeG, -1, 0);
+
+
+  JambeD = SDL_CreateWindow(
+      "Jambe droite", // codage en utf8, donc accents possibles
+      970, 740, // coin haut gauche en haut gauche de l'écran
+      100, 200, // largeur = 400, hauteur = 300
+      SDL_WINDOW_RESIZABLE);
+  if (JambeD == NULL)
+  {
+    SDL_Log("Error : SDL window 1 creation - %s\n",
+            SDL_GetError()); // échec de la création de la fenêtre
+    SDL_Quit();              // On referme la SDL
+    exit(EXIT_FAILURE);
+  }
+  RendJambeD = SDL_CreateRenderer(JambeD, -1, 0);
+
+  SDL_SetRenderDrawColor(RendTete, 255, 165, 0, 255);
+  SDL_SetRenderDrawColor(RendCou, 255, 165, 0, 255);
+  SDL_SetRenderDrawColor(RendBassin, 255, 165, 0, 255);
+  SDL_SetRenderDrawColor(RendCorps, 255, 165, 0, 255);
+  SDL_SetRenderDrawColor(RendBras, 255, 165, 0, 255);
+  SDL_SetRenderDrawColor(RendJambeG, 255, 165, 0, 255);
+  SDL_SetRenderDrawColor(RendJambeD, 255, 165, 0, 255);
+
+  SDL_RenderClear(RendCou);
+  SDL_Delay(20);
+
+  SDL_RenderClear(RendCorps);
+  SDL_Delay(20);
+  SDL_RenderPresent(RendCorps);
+
+  SDL_RenderClear(RendBassin);
+  SDL_Delay(20);
+  SDL_RenderPresent(RendBassin);
+
+  SDL_RenderClear(RendBras);
+  SDL_Delay(20);
+  SDL_RenderPresent(RendBras);
+
+    SDL_RenderClear(RendJambeG);
+  SDL_Delay(20);
+  SDL_RenderPresent(RendJambeG);
+
+  SDL_RenderClear(RendTete);
+  SDL_Delay(20);
+  SDL_RenderPresent(RendTete);
+
+  SDL_RenderClear(RendJambeD);
+  SDL_Delay(20);
+  SDL_RenderPresent(RendJambeD);
+  SDL_RenderPresent(RendCou);
+
+  SDL_Delay(3000);
+
+int *x, *y;
+*x=650;
+  while(*x+500 < 1820)
+  {
+    SDL_SetWindowPosition(JambeD, *x+10+320, *y);
+    SDL_SetWindowPosition(JambeG, *x+10+70, *y);
+    SDL_SetWindowPosition(Cou, *x+10+200, *y);
+    SDL_SetWindowPosition(Tete, *x+10+170, *y);
+    SDL_SetWindowPosition(Corps, *x+10+100, *y);
+    SDL_SetWindowPosition(Bassin, *x+10+70, *y);
+    SDL_SetWindowPosition(Bras, *x+10, *y);
+
+    SDL_GetWindowPosition(Bras, x, y);
+  }
+
+  /* et on referme tout ce qu'on a ouvert en ordre inverse de la création */
+  SDL_DestroyWindow(JambeG);  
+  SDL_DestroyWindow(JambeD);  
+  SDL_DestroyWindow(Bras);
+  SDL_DestroyWindow(Bassin);
+  SDL_DestroyWindow(Corps);
+  SDL_DestroyWindow(Cou);
+  SDL_DestroyWindow(Tete);
+
+  SDL_Quit(); // la SDL
+
+  return 0;
+}
