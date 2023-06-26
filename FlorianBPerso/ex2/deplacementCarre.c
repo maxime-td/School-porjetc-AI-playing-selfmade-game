ffbd1b4f84de9fd461043e50921e27b63f85c31f
Florian BOISSET <florian.boisset@etu.uca.fr>
2023-06-26 17:36:58 +0200

    deplacement carre noir sur fond orange

--- FlorianBPerso/ex2/deplacementCarre.c
+++ FlorianBPerso/ex2/deplacementCarre.c
@@ -10,11 +10,15 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    int continuer = 1;
+    int continuer = 1, direction = 0;
     SDL_Window
         *window = NULL;
     SDL_Renderer
         *renderer = NULL;
+
+    int x = 20, y = 20, width = 100, heigh = 100;
+    SDL_Rect carre = {x, y, width, heigh};
+
     SDL_Event event;
 
     /* Initialisation de la SDL  + gestion de l'échec possible */
@@ -29,7 +33,7 @@ int main(int argc, char **argv)
     window = SDL_CreateWindow(
         "Déplacements",        // codage en utf8, donc accents possibles
         650, 200,              // coin haut gauche en haut gauche de l'écran
-        400, 400,              // largeur = 400, hauteur = 300
+        800, 800,              // largeur = 400, hauteur = 300
         SDL_WINDOW_RESIZABLE); // redimensionnable
     if (window == NULL)
     {
@@ -40,22 +44,43 @@ int main(int argc, char **argv)
     }
 
     renderer = SDL_CreateRenderer(window, -1, 0);
+
     SDL_SetRenderDrawColor(renderer, 255, 165, 0, 255);
 
     while (continuer)
     {
+        SDL_SetRenderDrawColor(renderer, 255, 165, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_Delay(500);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &carre);
+        SDL_RenderPresent(renderer);
 
-        if(event.type == SDL_QUIT)
+        while (SDL_PollEvent(&event))
         {
-            SDL_Window *tmp = SDL_GetWindowFromID(event.window.windowID);
-            SDL_DestroyWindow(tmp); 
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+
+            case SDL_KEYUP:
+                if (event.key.keysym.sym == SDLK_UP)
+                    y -= 10;
+                if (event.key.keysym.sym == SDLK_RIGHT)
+                    x += 10;
+                if (event.key.keysym.sym == SDLK_LEFT)
+                    x -= 10;
+                break;
+            default:
+                y+=10;
+            }
         }
+        carre = {x, y, width, heigh};
     }
     SDL_RenderClear(renderer);
     SDL_Delay(20);
     SDL_RenderPresent(renderer);
-
-    SDL_Delay(3000);
 
     /* et on referme tout ce qu'on a ouvert en ordre inverse de la création */
     SDL_DestroyWindow(window);
