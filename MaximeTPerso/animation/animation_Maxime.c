e4a3baefdd646aef877c7a3a12f03aa1409ab5ba
mathoumin <maxime.thoumin@etu.uca.fr>
2023-06-27 06:43:46 +0200

    ajout play_with_texture_1 animation Maxime

--- MaximeTPerso/animation/animation_Maxime.c
+++ MaximeTPerso/animation/animation_Maxime.c
@@ -53,6 +53,30 @@ SDL_Texture* load_texture_from_image(char* file_image_name, SDL_Window *window, 
 }
 IMG_Quit(); // Si on charge une librairie SDL, il faut penser à la décharger  
 
+void play_with_texture_1(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer) {
+    SDL_Rect 
+        source = {0}, // Rectangle définissant la zone de la texture à récupérer
+        window_dimensions = {0}, // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
+        destination = {0}; // Rectangle définissant où la zone_source doit être déposée dans le renderer
+
+    SDL_GetWindowSize(
+    window, &window_dimensions.w,
+    &window_dimensions.h); // Récupération des dimensions de la fenêtre
+    SDL_QueryTexture(my_texture, NULL, NULL,
+             &source.w, &source.h); // Récupération des dimensions de l'image
+
+    destination = window_dimensions; // On fixe les dimensions de l'affichage à  celles de la fenêtre
+
+    /* On veut afficher la texture de façon à ce que l'image occupe la totalité de la fenêtre */
+    SDL_RenderCopy(renderer, my_texture,
+           &source,
+           &destination); // Création de l'élément à afficher
+    SDL_RenderPresent(renderer); // Affichage
+    SDL_Delay(2000); // Pause en ms
+
+    SDL_RenderClear(renderer); // Effacer la fenêtre
+}
+
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
