78059d6a5c5c59c12318f73afc61178c2ab15bc4
mathoumin <maxime.thoumin@etu.uca.fr>
2023-06-27 07:16:40 +0200

    ajout play_with_texture_3 et fonctionne animation Maxime

--- MaximeTPerso/animation/animation_Maxime.c
+++ MaximeTPerso/animation/animation_Maxime.c
@@ -98,6 +98,37 @@ void play_with_texture_2(SDL_Texture* my_texture, SDL_Window* window, SDL_Render
         SDL_RenderClear(renderer); // Effacer la fenêtre
 }
 
+void play_with_texture_3(SDL_Texture* my_texture, SDL_Window* window, SDL_Renderer* renderer) {
+    SDL_Rect 
+        source = {0}, // Rectangle définissant la zone de la texture à récupérer
+        window_dimensions = {0}, // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
+        destination = {0}; // Rectangle définissant où la zone_source doit être déposée dans le renderer
+                                
+    SDL_GetWindowSize(window, &window_dimensions.w, &window_dimensions.h); // Récupération des dimensions de la fenêtre
+    SDL_QueryTexture(my_texture, NULL, NULL, &source.w, &source.h); // Récupération des dimensions de l'image
+                                
+    /* On décide de déplacer dans la fenêtre cette image */
+    float zoom = 0.25; // Facteur de zoom entre l'image source et l'image affichée
+                                
+    int nb_it = 200; // Nombre d'images de l'animation
+    destination.w = source.w * zoom; // On applique le zoom sur la largeur
+    destination.h = source.h * zoom; // On applique le zoom sur la hauteur
+    destination.x = (window_dimensions.w - destination.w) / 2; // On centre en largeur
+    float h = window_dimensions.h - destination.h; // hauteur du déplacement à effectuer
+
+    for (int i = 0; i < nb_it; ++i) {
+      destination.y = h * (1 - exp(-5.0 * i / nb_it) / 2 *(1 + cos(10.0 * i / nb_it * 2 * M_PI))); // hauteur en fonction du numéro d'image
+
+      SDL_RenderClear(renderer); // Effacer l'image précédente
+
+      SDL_SetTextureAlphaMod(my_texture,(1.0-1.0*i/nb_it)*255); // L'opacité va passer de 255 à 0 au fil de l'animation
+      SDL_RenderCopy(renderer, my_texture, &source, &destination); // Préparation de l'affichage
+      SDL_RenderPresent(renderer); // Affichage de la nouvelle image
+      SDL_Delay(30); // Pause en ms
+    }                               
+    SDL_RenderClear(renderer); // Effacer la fenêtre une fois le travail terminé
+}
+
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
@@ -136,7 +167,7 @@ int main(int argc, char** argv) {
     my_texture = load_texture_from_image("fond-flammes.jpg", window, renderer);
     my_texture2 = load_texture_from_image("comcomdile.png", window, renderer);
     play_with_texture_1(my_texture, window, renderer);
-    play_with_texture_2(my_texture2, window, renderer);
+    play_with_texture_3(my_texture2, window, renderer);
 
     SDL_Delay(4000); // Pause exprimée en ms
                           
