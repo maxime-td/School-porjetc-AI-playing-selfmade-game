4c1852d59d89a53681d600d38b092e823aec9744
xerneas02 <mathis.roubille@gmail.com>
2023-06-27 13:35:39 +0200

    add val to struct sommet

--- src/graph.c
+++ src/graph.c
@@ -5,7 +5,7 @@
 #include "graph.h"
 
 /**
- * @brief Convertit un tableau de sommets en un graphe.
+ * @brief Convertit un tableau de sommets en un graphe connexe non cyclique.
  * @param tab Le tableau de sommets à convertir.
  * @param n Le nombre de sommets dans le tableau.
  * @return Un pointeur vers un sommet du graph
@@ -14,11 +14,15 @@ sommet_t * tabToGraph(sommet_t ** tab, int n){
     if(tab != NULL){
         srand(time(NULL));
         int cut = rand()%(n-1);
-        tab[0].voisins[0] = tabToGraph(tab+1, cut);
-        tab[0].n_voisin++;
+        tab[0]->voisins[tab[0]->n_voisin] = tabToGraph(tab+1, cut);
+        tab[1]->voisins[tab[1]->n_voisin] = tab[0];
+        tab[0]->n_voisin++;
+        tab[1]->n_voisin++;
         if(cut != 0){
-            tab[0].voisins[1] = tabToGraph(tab+cut, cut-n);
-            tab[0].n_voisin++;
+            tab[0]->voisins[tab[0]->n_voisin] = tabToGraph(tab+cut, cut-n);
+            tab[cut]->voisins[tab[cut]->n_voisin] = tab[0];
+            tab[0]->n_voisin++;
+            tab[cut]->n_voisin++;
         }
         return tab[0];
     }
