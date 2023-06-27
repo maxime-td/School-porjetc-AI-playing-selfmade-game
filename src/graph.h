5fd91b1dc798a4b6362c1c1815219266110bcafe
xerneas02 <mathis.roubille@gmail.com>
2023-06-27 19:13:34 +0200

    affichage dans autres fichier+ fix distance point

--- src/graph.h
+++ src/graph.h
@@ -4,7 +4,7 @@
 #include <SDL2/SDL.h>
 
 #define N 12 // NOMBRE_MAX_SOMMET 
-#define R 200 //Rayon cercle repartition
+#define R 300 //Rayon cercle repartition
 #define W 800
 #define H 800
 
@@ -71,29 +71,11 @@ int calculDistance(sommet_t * a, sommet_t * b);
 int ** distTab(sommet_t ** tab, int * n);
 
 /**
- * @brief Trace un disque
- * @param renderer Le renderer où tracer le disque
- * @param center_x La coordonnée x du centre du disque
- * @param center_y La coordonnée y du centre du disque
- * @param radius Le rayon du disque
- */
-
-/**
  * @brief Affiche le tableau des distances
  * @param distTab le tableau des distances
  * @param n le pointeur sur le ombre de sommets
  */
 void printDistTab(int ** distTab, int * n);
 
-
-void draw_disk(SDL_Renderer* renderer, int center_x, int center_y, int radius);
-
-/**
- * @brief Dessine un graphe à l'aide d'un rendu SDL.
- * @param renderer Le rendu SDL utilisé pour afficher le graphe.
- * @param graph Le pointeur vers le graphe à dessiner.
- * @param n Le nombre de sommets dans le tableau.
- */
-void drawGraph(SDL_Renderer* renderer, sommet_t** tab, int n);
 
 #endif /* GRAPH_H_ */
--- src/main.c
+++ src/main.c
@@ -1,93 +1,24 @@
 #include <stdio.h>
 #include <time.h>
 #include "graph.h"
+#include "affiche.h"
 
 int main()
 {
     srand(time(NULL));
     int n=0;
     sommet_t ** tab = genTabSommets(&n, 800, 800);
-    printTabCoord(tab, &n);
-    printf("--------------------------------------------\n");
+    //printTabCoord(tab, &n);
+    //printf("--------------------------------------------\n");
     tabToGraph(tab, 0, n-1);
-    printTabCoord(tab, &n);
+    //printTabCoord(tab, &n);
 
     makeNewLinks(5, tab, &n);
-    printTabCoord(tab, &n);
+    //printTabCoord(tab, &n);
     int ** TableauDistances = distTab(tab, &n);
     printDistTab(TableauDistances, &n);
-    int y = 0, x;
-    int width, height, w_window = 800, h_window = 800, final_width = w_window/2;
-    SDL_bool program_on = SDL_TRUE;
-    SDL_Event event;   
 
-    SDL_DisplayMode dm;
-    SDL_Renderer* renderer;
-
-    SDL_Window *window = NULL;
-
-    
-
-    /* Initialisation de la SDL  + gestion de l'échec possible */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        SDL_Log("Error : SDL initialisation - %s\n",
-                SDL_GetError()); // l'initialisation de la SDL a échoué
-        exit(EXIT_FAILURE);
-    }
-
-    /* recupere la taille de l'ecran */
-    if (SDL_GetDesktopDisplayMode(0, &dm) != 0)
-    {
-        SDL_Log("SDL_GetDesktopDisplayMode failed: %s", SDL_GetError());
-        return 1;
-    }
-
-    width = dm.w;
-    height = dm.h;
-
-    /* Création de la fenêtre de gauche */
-    window = SDL_CreateWindow(
-        "Goutte",    // codage en utf8, donc accents possibles
-        width/2-w_window/2, 0,                  // coin haut gauche en haut gauche de l'écran
-        w_window, h_window,              // largeur = 400, hauteur = 300
-        SDL_WINDOW_RESIZABLE);
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 250);
-    SDL_RenderClear(renderer);
-    
-
-    if (window == NULL)
-    {
-        SDL_Log("Error : SDL window 1 creation - %s\n",
-                SDL_GetError()); // échec de la création de la fenêtre
-        SDL_Quit();              // On referme la SDL
-        exit(EXIT_FAILURE);
-    }
-
-    SDL_GetWindowPosition(window, &x, &y);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 250);
-    SDL_Delay(100);
-    drawGraph(renderer, tab, n);
-    
-    SDL_RenderPresent(renderer);
-
-    while (program_on)
-    {
-        /* Gestion des événements */
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-                case SDL_QUIT:
-                    program_on = SDL_FALSE;
-                    break;
-                default :
-                    break;
-            }
-        }
-    }
+    affiche(tab, n);
 
     return 0;
 }
