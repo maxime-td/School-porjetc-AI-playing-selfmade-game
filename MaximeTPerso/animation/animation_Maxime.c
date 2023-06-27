3edf0d39a18e2efbad585275e8e8d7dd05ef65ac
mathoumin <maxime.thoumin@etu.uca.fr>
2023-06-27 06:38:15 +0200

    ajout SDL_Texture dans animation Maxime

--- MaximeTPerso/animation/animation_Maxime.c
+++ MaximeTPerso/animation/animation_Maxime.c
@@ -36,6 +36,23 @@ void end_sdl(char ok,                            // fin normale : ok = 0 ; anorm
         }                                                          
 }
 
+SDL_Texture* load_texture_from_image(char* file_image_name, SDL_Window *window, SDL_Renderer *renderer) {
+    SDL_Surface *my_image = NULL;           // Variable de passage
+    SDL_Texture* my_texture = NULL;         // La texture
+
+    my_image = IMG_Load(file_image_name);   // Chargement de l'image dans la surface
+                                            // image=SDL_LoadBMP(file_image_name); fonction standard de la SDL, 
+                                            // uniquement possible si l'image est au format bmp */
+    if (my_image == NULL) end_sdl(0, "Chargement de l'image impossible", window, renderer);
+   
+    my_texture = SDL_CreateTextureFromSurface(renderer, my_image); // Chargement de l'image de la surface vers la texture
+    SDL_FreeSurface(my_image); // la SDL_Surface ne sert que comme élément transitoire 
+    if (my_texture == NULL) end_sdl(0, "Echec de la transformation de la surface en texture", window, renderer);
+
+    return my_texture;
+}
+IMG_Quit(); // Si on charge une librairie SDL, il faut penser à la décharger  
+
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
