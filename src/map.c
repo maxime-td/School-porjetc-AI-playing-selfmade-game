5edadc9349bfe931784dbb9a7ba96f5e5777fe60
Florian BOISSET <florian.boisset@etu.uca.fr>
2023-06-29 11:34:08 +0200

    asteroid partout

--- src/map.c
+++ src/map.c
@@ -2,35 +2,51 @@
 #include "graph.h"
 #include "affiche.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 
 
 void ast_Partout(SDL_Renderer * render)
 {
-    int frame_size = 54;
+    int frame_size = 48;
      SDL_Surface
-        *surf = IMG_Load("../images/planetes.png");
+        *surf = IMG_Load("./images/planetes.png");
 
     SDL_Texture *texture = NULL;
+
+    int alea=0;
+    int angle=0;
+    int ecart= (rand()%20)-10;
 
     SDL_Rect srcrect = {0, 0, frame_size, frame_size}; // position and size of the part of the image to draw
     SDL_Rect dstrect = {100, 100, 54, 54};           // position and size of the destination on the screen
 
     texture = SDL_CreateTextureFromSurface(render, surf);
     
-    for(int i=0; i<W; i+=54)
+    for(int i=0; i<W; i+=frame_size+ecart)
     {
+        ecart= (rand()%20)-10;
         dstrect.x=i;
-        for(int j=0; j<H; j+=54)
+        for(int j=0; j<H; j+=frame_size+ecart)
         {
+            ecart= (rand()%20)-10;
             dstrect.y=j;
-            srcrect.x = 54*rand()%16;
-            SDL_RenderCopy(render, texture, &srcrect, &dstrect);
+            alea = (rand()%16)*48;
+            printf("%d\n", alea);
+            srcrect.x = alea;
+            angle = rand()%360;
+            SDL_RenderCopyEx(render, texture, &srcrect, &dstrect, angle, NULL, 0);
         }
     }
     SDL_RenderPresent(render); 
 
-    SDL_Delay(6000);
+    //SDL_Delay(3000);
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(surf);
 }
+
+/*
+void passage_Ast(SDL_Renderer * render, sommet_t ** )
+{
+
+}*/
