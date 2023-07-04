a5d595a91dd1c5044031c139b3bc9587e3f0d02c
Florian BOISSET <florian.boisset@etu.uca.fr>
2023-07-04 08:59:39 +0200

    début modification fichiers threads

--- src/bouclesJeu.c
+++ src/bouclesJeu.c
@@ -15,39 +15,7 @@
 #include "OptiFloyd_Warshall.h"
 #include "bouclesJeu.h"
 #include "map.h"
-
-/**
- * @brief fonction de lancement de thread pour l'algorithme des fourmis
- * @param args Une structure contenant tout les argument a donner a multi_start_fourmi
-*/
-void *thread_fourmi(FourmiArgs *args)
-{
-    args->result = multi_start_fourmi(args->matDist, args->n);
-    return NULL;
-}
-
-
-/**
- * @brief fonction de lancement de thread pour l'algorithme des floyd
- * @param args Une structure contenant tout les argument a donner a multi_Start_Floyd_Warshall
-*/
-void *thread_floyd(FloydWarshallArgs *args)
-{
-    args->result = multi_Start_Floyd_Warshall(args->tabWarshall, args->tabDist, args->n);
-    return NULL;
-}
-
-/**
- * @brief fonction de lancement de thread pour le timer du jeu
- * @param args Une structure contenant tout les argument necessaire au timer et à la recuperation de ce timer
-*/
-void *timer(timerArgs* timer){
-    while((*timer->fin)){
-        SDL_Delay(10);
-        (timer->time)++;
-    }
-    return NULL;
-}
+#include "threads.h"
 
 /**
  * @brief fonction de lancement de thread pour l'affichage graphique du jeu
--- src/bouclesJeu.h
+++ src/bouclesJeu.h
@@ -12,6 +12,7 @@
 #include "graph.h"
 #include "affiche.h"
 #include "fourmi.h"
+#include "thredas.h"
 
 #define MAX_SPEED 0.003
 #define ACCELERATION 0.000000025
@@ -19,90 +20,6 @@
 
 #define N_RULE 3
 #define TIME_MAX_IA 40
-
-// Structure pour les arguments de multi_start_fourmi
-typedef struct coordonne_s{
-    int x;
-    int y;
-} coordonne_t;
- 
-// Structure pour les arguments de multi_start_fourmi
-typedef struct FourmiArgs_s{
-    int** matDist;
-    int n;
-    int result;
-} FourmiArgs;
-
-// Structure pour les arguments de cycle_Floyd_Warshall
-typedef struct {
-    int** tabWarshall;
-    int** tabDist;
-    int n;
-    int result;
-} FloydWarshallArgs;
-
-// Structure pour les arguments de cycle_Floyd_Warshall
-typedef struct {
-    SDL_bool * fin;
-    int time;
-} timerArgs;
-
-// Structure pour les arguments de affiche
-typedef struct afficheArgs_s{
-    int * count;
-    int frame;
-    int frameEF;
-    int frameFlag;
-    int frameTN;
-    int type_fin;
-    SDL_Rect etoile;
-    SDL_Rect etoileFilante;
-    SDL_Rect background;
-    SDL_Rect navette;
-    SDL_Rect planete;
-    SDL_Rect flag;
-    SDL_Rect affTrouNoir;
-    asteroid_t * asteroid;
-    SDL_Texture * texture;
-    SDL_Texture * textureBg;
-    SDL_Texture * textureE1;
-    SDL_Texture * textureE2;
-    SDL_Texture * textureEF;
-    SDL_Texture * textureP;
-    SDL_Texture * textureF;
-    SDL_Texture * textureTN;
-    int * fin;
-    int * planeteVisite;
-    sommet_t ** sous_graphe;
-    sommet_t ** tab;
-    int n_sous_graphe;
-    int n;
-    int n_ast;
-    SDL_bool * program_on;
-    int * chemin;
-    coordonne_t * co;
-    double * x;
-    double * y;
-}afficheArgs;
-
-
-/**
- * @brief fonction de lancement de thread pour l'algorithme des fourmis
- * @param args Une structure contenant tout les argument a donner a multi_start_fourmi
-*/
-void *thread_fourmi(FourmiArgs *args);
-
-/**
- * @brief fonction de lancement de thread pour l'algorithme des floyd
- * @param args Une structure contenant tout les argument a donner a multi_Start_Floyd_Warshall
-*/
-void *thread_floyd(FloydWarshallArgs *args);
-
-/**
- * @brief fonction de lancement de thread pour le timer du jeu
- * @param args Une structure contenant tout les argument necessaire au timer et à la recuperation de ce timer
-*/
-void *timer(timerArgs* timer);
 
 /**
  * @brief fonction de lancement de thread pour l'affichage graphique du jeu
